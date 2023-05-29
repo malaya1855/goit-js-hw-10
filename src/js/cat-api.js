8a556277edd5ea3a6ae42552b0ac1db0bb88039e
malaya1855 <121491813+malaya1855@users.noreply.github.com>
2023-05-29 21:00:23 +0100

    export-import

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -1,23 +1,27 @@
-export default function fetchBreeds() {
-    const BASE_URL = "https://api.thecatapi.com"
-const options = {
-headers: {
-"x-api-key": "live_4ebVElgqYTCESdDh4RFJDglzgslxsUSiTF80DOwUuLu9mvV9OJec6LfoFoweFznb",
-}}
+export default class NewApiService {
+    constructor () {
+         this.BASE_URL = "https://api.thecatapi.com"
+         this.options = {
+            headers: {
+            "x-api-key": "live_4ebVElgqYTCESdDh4RFJDglzgslxsUSiTF80DOwUuLu9mvV9OJec6LfoFoweFznb",
+        }}
+        this.breedId = '';
 
-    return fetch(`${BASE_URL}/v1/breeds`, options)
-    .then(response => response.json())
-}
-
-
-function renderBreeds(cats) {
-    catsList = cats;
-    for (let i = 0; i < cats.length; i += 1){
-        const cat = cats[i];
-       const breedOption = document.createElement("option")
-       breedOption.value = cat.id;
-       breedOption.innerHTML = `${cat.name}`;
-       selectBreeds.appendChild(breedOption)
     }
-}
+fetchBreeds() {
+        return fetch(`${this.BASE_URL}/v1/breeds`, this.options)
+        .then(response => response.json())
+    }
 
+fetchCatByBreed(breedId){
+        return fetch (`${this.BASE_URL}/v1/images/search?breed_ids=${this.breedId}`, this.options)
+        .then(response => response.json())
+    }
+    get breed (){
+        return this.breedId;
+    }
+    set breed (newBreedId){
+        this.breedId = newBreedId
+    }
+
+}
--- src/js/index.js
+++ src/js/index.js
@@ -1,37 +1,35 @@
 
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { Report } from 'notiflix/build/notiflix-report-aio'
 import SlimSelect from 'slim-select'
-// import fetchBreeds from './cat-api';
+import "slim-select/dist/slimselect.css";
+import NewApiService from './cat-api';
 
-const BASE_URL = "https://api.thecatapi.com"
-const options = {
-    headers: {
-    "x-api-key": "live_4ebVElgqYTCESdDh4RFJDglzgslxsUSiTF80DOwUuLu9mvV9OJec6LfoFoweFznb",
-}}
 
 const selectBreeds = document.querySelector("select.breed-select")
 const errorMessage = document.querySelector('.error')
 const catCard = document.querySelector('.cat-info')
 const loaderMark = document.querySelector('.loader') 
-let breedId = '';
+
+const newApiService = new NewApiService()
 
 errorMessage.classList.add('visually-hidden')
+selectBreeds.classList.add('visually-hidden')
+catCard.classList.add('visually-hidden') 
 
-fetchBreeds()
+newApiService.fetchBreeds()
 .then((cats) => {
     loaderMark.classList.add('visually-hidden')
     selectBreeds.classList.remove('visually-hidden')
-    renderBreeds(cats)})
+    renderBreeds(cats)
+    })
 .catch(error => {
-    selectBreeds.classList.add('visually-hidden')
-    loaderMark.classList.remove('visually-hidden')
-    Notify.failure(errorMessage.textContent)})
-
-function fetchBreeds() {
-    selectBreeds.classList.add('visually-hidden')
-    return fetch(`${BASE_URL}/v1/breeds`, options)
-    .then(response => response.json())
-}
+    errorMessage.classList.remove('visually-hidden')
+    Report.failure(
+        'Oops! Something went wrong!',
+        'Try reloading the page!',
+        'Okay',
+        );
+})
 
 function renderBreeds(cats) {
     const breedItem = cats.map(
@@ -40,47 +38,46 @@ function renderBreeds(cats) {
             breedOption.value = cat.id;
             breedOption.innerHTML = `${cat.name}`
             return breedOption
-        }
-        )
+        })
         selectBreeds.append(...breedItem);
         
-        // new SlimSelect({
-        //     select: selectBreeds,
-            
-        // })
+        new SlimSelect({
+            select: selectBreeds,
+        })
     }
     
-    selectBreeds.addEventListener("change", onCreateCatCard);
+selectBreeds.addEventListener("change", onCreateCatCard);
 
-    function onCreateCatCard (e){
+function onCreateCatCard(e) {
 e.preventDefault();
+newApiService.breedId = e.currentTarget.value;
 
-fetchCatByBreed()
-    .then(catCards => {
+loaderMark.classList.remove('visually-hidden')
+catCard.classList.add('visually-hidden') 
+
+newApiService.fetchCatByBreed()
+.then(catCards => {
         loaderMark.classList.add('visually-hidden')
         catCard.classList.remove('visually-hidden'),
         renderCatInfo(catCards)})
-    .catch(error => {
-        catCard.classList.add('visually-hidden')
-    loaderMark.classList.remove('visually-hidden')
-        Notify.failure(errorMessage.textContent)})
-    
-    function fetchCatByBreed(breedId){
-        breedId = e.currentTarget.value;
-        catCard.classList.add('visually-hidden')
-        loaderMark.classList.remove('visually-hidden')
-        return fetch (`${BASE_URL}/v1/images/search?breed_ids=${breedId}`, options)
-        .then(response => response.json())
+.catch(error => {
+        errorMessage.classList.remove('visually-hidden')
+    Report.failure(
+        'Oops! Something went wrong!',
+        'Try reloading the page!',
+        'Okay',
+        );
+    })
+
+function renderCatInfo(catCards){
+        for (let i = 0; i < catCards.length; i += 1){
+                const cat = catCards[0];
+                catCard.innerHTML = `<img class="image" src="${cat.url}" alt="${cat.breeds[0].name} width=500">
+                <div>
+                <h2 class="name">${cat.breeds[0].name}</h2>
+                <p class="description">${cat.breeds[0].description}</p>
+                <p class="temperament"><span class="title">Temperament:</span> ${cat.breeds[0].temperament}</p>
+                </div>`      
+            }
+            }  
     }
-    function renderCatInfo(catCards){
-for (let i = 0; i < catCards.length; i += 1){
-        const cat = catCards[0];
-        catCard.innerHTML = `<img class="image" src="${cat.url}" alt="${cat.breeds[0].name}">
-        <div>
-        <h2 class="name">${cat.breeds[0].name}</h2>
-        <p class="description">${cat.breeds[0].description}</p>
-        <p class="temperament"><span class="title">Temperament:</span> ${cat.breeds[0].temperament}</p>
-        </div>`
-        
-    }
-    }}
